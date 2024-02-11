ccad4cdc3403476c3e640cc14342b6be16ae1100
Samuel Adu <samuel.adu@student.rocva.nl>
2024-02-11 06:17:43 +0100

    producten

--- resources/views/home/producten.blade.php
+++ resources/views/home/producten.blade.php
@@ -24,7 +24,7 @@
         <div class="max-w-6xl px-5 mx-auto mt-28 text-center">
             <div class="grid md:grid-cols-4 gap-6 sm:grid-cols-2 gap-6">
                 @foreach($producten as $product)
-                <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow hover:border-indigo-600 dark:border-gray-700">
+                <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow hover:border-indigo-600">
                     <!-- Productlink met afbeelding -->
                     <a href="{{url('product', $product->slug)}}">
                         <img class="rounded-t-lg" src="/product/{{$product->foto}}" alt="product image" />
@@ -34,7 +34,7 @@
                     <div class="px-5 pb-5">
                         <!-- Productnaam -->
                         <a href="{{url('product', $product->slug)}}">
-                            <h5 class="text-lg font-semibold tracking-tight text-gray-900 dark:text-black mt-2">{{$product->naam}}</h5>
+                            <h5 class="text-lg font-semibold tracking-tight text-gray-900 mt-2">{{$product->naam}}</h5>
                         </a>
 
                         <!-- Beoordeling en waardering -->
@@ -53,11 +53,11 @@
                                 <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
                                     <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
                                 </svg>
-                                <svg class="w-4 h-4 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                <svg class="w-4 h-4 text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
                                     <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
                                 </svg>
                                 <!-- Gemiddelde beoordeling -->
-                                <span class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">4.3</span>
+                                <span class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded ms-3">4.3</span>
                             </div>
                         </div>
 
@@ -65,12 +65,12 @@
                         <div class="flex items-center justify-between">
                             @if($product->korting_prijs != null)
                             <!-- Prijs met korting -->
-                            <span class="text-3xl font-bold text-gray-900 dark:text-black ">€{{$product->korting_prijs}}</span>
+                            <span class="text-3xl font-bold text-gray-900 ">€{{$product->korting_prijs}}</span>
                             <!-- Oorspronkelijke prijs (doorstreept) -->
                             <span class="text-3xl font-bold text-red-600 line-through decoration-red-600">€{{$product->prijs}}</span>
                             @else
                             <!-- Standaardprijs zonder korting -->
-                            <span class="text-3xl font-bold text-gray-900 dark:text-black">€{{$product->prijs}}</span>
+                            <span class="text-3xl font-bold text-gray-900">€{{$product->prijs}}</span>
                             @endif
                         </div>
                     </div>
