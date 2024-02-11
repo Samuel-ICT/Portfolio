445b1fa57d38f12beb0f6fe67cca0abc11731950
Samuel Adu <samuel.adu@student.rocva.nl>
2024-02-11 06:15:44 +0100

    heren

--- resources/views/home/heren.blade.php
+++ resources/views/home/heren.blade.php
@@ -27,7 +27,7 @@
                 @foreach($heren as $producten)
                 <!-- Individueel productblok -->
                 <div
-                    class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow hover:border-indigo-600 dark:border-gray-700">
+                    class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow hover:border-indigo-600">
                     <!-- Koppeling naar productdetailpagina -->
                     <a href="{{url('product', $producten->slug)}}">
                         <img class="rounded-t-lg" src="/product/{{$producten->foto}}" alt="product image" />
@@ -36,7 +36,7 @@
                     <div class="px-5 pb-5">
                         <!-- Koppeling naar productdetailpagina -->
                         <a href="{{url('product', $producten->slug)}}">
-                            <h5 class="text-lg font-semibold tracking-tight text-gray-900 dark:text-black mt-2">
+                            <h5 class="text-lg font-semibold tracking-tight text-gray-900 mt-2">
                                 {{$producten->naam}}</h5>
                         </a>
                         <!-- Beoordelingssterren -->
@@ -62,7 +62,7 @@
                                     <path
                                         d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
                                 </svg>
-                                <svg class="w-4 h-4 text-gray-200 dark:text-gray-600" aria-hidden="true"
+                                <svg class="w-4 h-4 text-gray-200" aria-hidden="true"
                                     xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
                                     <path
                                         d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
@@ -70,7 +70,7 @@
                             </div>
                             <!-- Beoordelingsscore -->
                             <span
-                                class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">4.3
+                                class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded ms-3">4.3
                             </span>
                         </div>
                         <!-- Productprijsinformatie -->
@@ -78,13 +78,13 @@
                             @if($producten->korting_prijs!=null)
                             <!-- Prijs met korting -->
                             <span
-                                class="text-3xl font-bold text-gray-900 dark:text-black ">€{{$producten->korting_prijs}}</span>
+                                class="text-3xl font-bold text-gray-900 ">€{{$producten->korting_prijs}}</span>
                             <!-- Oorspronkelijke prijs -->
                             <span
                                 class="text-3xl font-bold text-red-600 line-through decoration-red-600">€{{$producten->prijs}}</span>
                             @else
                             <!-- Standaardprijs zonder korting -->
-                            <span class="text-3xl font-bold text-gray-900 dark:text-black">€{{$producten->prijs}}</span>
+                            <span class="text-3xl font-bold text-gray-900">€{{$producten->prijs}}</span>
                             @endif
                         </div>
                     </div>
