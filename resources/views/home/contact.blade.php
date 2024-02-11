a062b65fcd7135570518eb52b41619adbe40c2d8
Samuel Adu <samuel.adu@student.rocva.nl>
2024-02-11 06:14:51 +0100

    contactpagina

--- resources/views/home/contact.blade.php
+++ resources/views/home/contact.blade.php
@@ -24,7 +24,7 @@
         <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
             <!-- Titel en inleidende tekst -->
             <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900">Contact Ons</h2>
-            <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Heeft u vragen
+            <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 sm:text-xl">Heeft u vragen
                 of opmerkingen, wilt u een afspraak maken of wilt u meer weten over onze producten? Neem dan
                 contact met ons op via onderstaand formulier
             </p>
@@ -32,7 +32,7 @@
             <!-- Toon succesbericht als session('message') bestaat -->
             @if(session('message'))
             <div id="alert-3"
-                class="flex items-center mt-5 p-4 mb-4 text-green-800 rounded-lg bg-green-50 dark:bg-green-200 dark:text-green-400"
+                class="flex items-center mt-5 p-4 mb-4 text-green-800 rounded-lg bg-green-50"
                 role="alert">
                 <!-- Succesicoon en bericht -->
                 <svg class="flex-shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
@@ -46,7 +46,7 @@
                 </div>
                 <!-- Sluitknop -->
                 <button type="button"
-                    class="ml-auto -mx-1.5 -my-1.5 bg-green-50 text-green-500 rounded-lg focus:ring-2 focus:ring-green-400 p-1.5 hover:bg-green-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-green-400 dark:hover:bg-gray-700"
+                    class="ml-auto -mx-1.5 -my-1.5 bg-green-50 text-green-500 rounded-lg focus:ring-2 focus:ring-green-400 p-1.5 hover:bg-green-200 inline-flex items-center justify-center h-8 w-8"
                     data-dismiss-target="#alert-3" aria-label="Sluiten">
                     <span class="sr-only">Sluiten</span>
                     <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
@@ -65,7 +65,7 @@
                 <div>
                     <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Email</label>
                     <input type="email" id="email" name="email"
-                        class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-indigo-500 dark:border-gray-600 dark:placeholder-gray-300 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                        class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
                         placeholder="naam@gmail.com" required>
                 </div>
 
@@ -73,7 +73,7 @@
                 <div>
                     <label for="onderwerp" class="block mb-2 text-sm font-medium text-gray-900">Onderwerp</label>
                     <select name="onderwerp" id="onderwerp"
-                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-indigo-500 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
                         <option disabled selected>Kies een onderwerp uit</option>
                         <option value="v&b">Verzending & Bezorging</option>
                         <option value="retourneren">Retourneren</option>
@@ -86,13 +86,13 @@
                 <div class="sm:col-span-2">
                     <label for="message" class="block mb-2 text-sm font-medium text-gray-900">Bericht</label>
                     <textarea name="bericht" id="message" rows="6"
-                        class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-indigo-500 dark:border-gray-600 dark:placeholder-gray-300 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                        class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500"
                         placeholder="Laat hier uw bericht achter..."></textarea>
                 </div>
 
                 <!-- Verzendknop -->
                 <button type="submit" name="submit"
-                    class="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-indigo-500 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Verzend
+                    class="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-indigo-500 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300">Verzend
                     bericht</button>
             </form>
         </div>
