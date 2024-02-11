a5a88c23f32543760d6f6be1ec29e617c54dea07
Samuel Adu <samuel.adu@student.rocva.nl>
2024-02-11 06:10:39 +0100

    users

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -21,7 +21,7 @@
 
         <!-- Tabel met gebruikersgegevens -->
         <div class="flex items-center sm:rounded-lg md:ml-64">
-            <table class="text-sm text-left text-gray-500 dark:text-gray-400">
+            <table class="text-sm text-left text-gray-500">
                 <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
                         <th scope="col" class="px-4 py-3">
@@ -48,7 +48,7 @@
                 <!-- Tabelinhoud met gebruikersgegevens -->
                 <tbody>
                     @foreach($users as $user)
-                    <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                         <th scope="row" class="px-3 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                             {{$user->name}}
                         </th>
@@ -66,9 +66,7 @@
                         </td>
                         <td class="px-6 py-4">
                             <!-- Verwijderen van gebruiker met bevestigingsprompt -->
-                            <a href="{{url('admin/users/verwijderen',$user->id)}}"
-                                onclick="return confirm('Ben je zeker of je dit wilt verwijderen?')"
-                                class="font-medium text-red-600 dark:text-red-500 hover:underline">Verwijderen</a>
+                            <a href="{{url('admin/users/verwijderen',$user->id)}}" onclick="return confirm('Ben je zeker of je dit wilt verwijderen?')" class="font-medium text-red-600 hover:underline">Verwijderen</a>
                         </td>
                     </tr>
                     @endforeach
